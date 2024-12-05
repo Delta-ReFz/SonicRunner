76b5e091a61b244dae003ed19f89f21c85eb2b4f
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2024-12-05 16:15:13 -0500

    background is now moving

--- scenes/mainMenu.js
+++ scenes/mainMenu.js
@@ -6,4 +6,24 @@ export default function mainMenu() {
     k.onButtonPress("jump", () => k.go("game"));
 
     const bgPieceWidth = 1920;
+    const bgPieces = [
+        k.add([k.sprite("chemical-bg"), k.pos(0, 0), k.scale(2), k.opacity(0.8), k.area()]),
+        k.add([k.sprite("chemical-bg"), k.pos(bgPieceWidth * 2, 0), k.scale(2), k.opacity(0.8), k.area()]),
+    ];
+
+    const platforWidth = 1280;
+    const platforms = [
+        k.add([k.sprite("platforms"), k.pos(0,450), k.scale(4) ]),
+        k.add([k.sprite("platforms"), k.pos(platforWidth * 4, 450), k.scale(4) ])
+    ];
+
+    k.onUpdate(() => {
+        if(bgPieces[1].pos.x < 0) {
+            bgPieces[0].moveTo(bgPieces[1].pos.x + bgPieceWidth * 2, 0);
+            bgPieces.push(bgPieces.shift()); 
+        }
+
+        bgPieces[0].move(-100, 0)
+        bgPieces[1].moveTo(bgPieces[0].pos.x + bgPieceWidth * 2, 0);
+    });
 }
